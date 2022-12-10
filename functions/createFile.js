edf1849d5fb6b150e128f716f052a58e95e17cfb
Spyei <caio.salamao1234@gmail.com>
2022-12-10 11:22:06 -0300

    primeira commit

---
+++ functions/createFile.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+function createFile(filename, content){
+    let data = content || "data"
+    let createFile = fs.writeFile(filename, data, (err) => {
+        if (err) throw err;
+            console.log("Created file with name: " + filename)
+        })
+    return createFile
+}
+
+module.exports = {
+    createFile
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+const { createFile } = require("./functions/createFile")
