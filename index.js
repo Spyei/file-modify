0ae21ca126de39595ec5e89150f45a41107a95ce
Spyei <caio.salamao1234@gmail.com>
2022-12-12 10:06:19 -0300

    excluição da pasta fdfsdf

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const { createFile } = require("./functions/createFile")
-const { deleteFile } = require("./functions/deleteFile")
+const fs = require("fs")
+
+function createFile(filename, content){
+    let data = content || ""
+    let createFile = fs.writeFile(filename, data, (err) => {
+        let nome = filename ||"porra"
+        if(nome == "porra") return console.log("Enter the name of the file you want to create.") 
+    })
+    return createFile
+}
+
+function deleteFile(filename){
+    let nome = filename || "porra"
+    let deletefile = fs.unlink(filename, (err) => {
+        if(nome == "porra") return console.log("Enter the name of the file you want to delete.")
+    })
+    return deleteFile
+}
+
+module.exports = {
+    deleteFile: deleteFile,
+    createFile: createFile
+}
